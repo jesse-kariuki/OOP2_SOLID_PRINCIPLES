023a94b2c6d71e9201910467bfe6c03582b0f274
happy-wondessen <happywondessen@gmail.com>
2025-06-12 09:52:15 +0300

    Added the Simple_Robot class

--- src/Interface_Segregation_Principle/Robot.java
+++ src/Interface_Segregation_Principle/Robot.java
@@ -18,11 +18,7 @@ public class Robot implements Worker {
 }
 
 /*
-public class Robot implements Workable {
-    @Override
-    public void work() {
-        System.out.println("Robot is working");
-    }
+
 }
 
  */
